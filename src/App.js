290f57488c15812f7fc7a4048b444dcef82a8c08
Teffy English <teffy.english@gmail.com>
2022-08-30 19:02:48 -0500

    Last

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import colorArray from './colorArray.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
-
 let quotesDB = "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
 
 function App() {
